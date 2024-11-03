923c5aea7ef3b933fe117192007400651872bad9
dukc <dukkcc@proton.me>
2024-11-03 15:44:26 -0800

    started blog

---
+++ src/content/blog/problem-of-copyright copy copy copy.md
@@ -0,0 +1,9 @@
+---
+  title: "The Problem of Copyright in the Bible Industry"
+  date: 2024-08-08
+  author: "dukc"
+  image: "/blog/exploiting-jesus.jpg"
+  draft: false
+---
+
+Draft blog.
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,22 +5,4 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   presets: [bibleioTheme],
   darkMode: 'class',
-  theme: {
-    extend: {
-      keyframes: {
-        scrollY: {
-          '0%, 100%': { transform: 'translateY(0)' },
-          '50%': { transform: 'translateY(1)' },
-        },
-        scrollX: {
-          '0%, 100%': { transform: 'translateX(0)' },
-          '50%': { transform: 'translateX(1)' },
-        },
-      },
-      animation: {
-        scrollY: 'scrollY 25s linear infinite',
-        scrollX: 'scrollX 25s linear infinite',
-      },
-    },
-  },
 };
