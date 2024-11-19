bd4bc3f15b3f289bc810bda863f24cbbed3a30f8
dukc <dukkcc@proton.me>
2024-11-18 18:30:12 -0800

    add sitemap & analytics

--- apps/website/astro.config.mjs
+++ apps/website/astro.config.mjs
@@ -5,10 +5,25 @@ import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
 
+import yeskunallumami from "@yeskunall/astro-umami";
+
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [react(), tailwind()],
-	devToolbar: {
-		enabled: false,
-	},
+  site: "https://www.bibleio.com",
+  integrations: [
+    react(),
+    tailwind(),
+    yeskunallumami({
+      // id: import.meta.env.UMAMI_SITE_ID,
+      // endpointUrl: import.meta.env.UMAMI_ENDPOINT_URL,
+      id: "3f919d73-0804-4662-b4c6-43e1551fe508",
+      endpointUrl: "https://umami.cool.duckyhq.com",
+    }),
+    sitemap(),
+  ],
+  devToolbar: {
+    enabled: false,
+  },
 });
