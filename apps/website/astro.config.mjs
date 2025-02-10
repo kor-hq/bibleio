4b7841a17e84c20ee984eec2c737fbb2e8018929
dukc <dukkcc@proton.me>
2025-02-10 08:26:58 -0800

    start switching to unpreview, tailwind v4

--- apps/website/astro.config.mjs
+++ apps/website/astro.config.mjs
@@ -2,25 +2,24 @@
 import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
-
-import tailwind from "@astrojs/tailwind";
-
 import yeskunallumami from "@yeskunall/astro-umami";
-
 import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://www.bibleio.com",
 	integrations: [
 		react(),
-		tailwind({ applyBaseStyles: false }),
 		yeskunallumami({
 			id: "3f919d73-0804-4662-b4c6-43e1551fe508",
 			endpointUrl: "https://umami.cool.korhq.com",
 		}),
 		sitemap(),
 	],
+	vite: {
+		plugins: [tailwindcss()],
+	},
 	devToolbar: {
 		enabled: false,
 	},
@@ -32,8 +31,5 @@ export default defineConfig({
 				hostname: "**.amazonaws.com",
 			},
 		],
-	},
-	experimental: {
-		contentLayer: true,
 	},
 });
