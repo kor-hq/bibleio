acfefc7fc888bfaf2311bcbcaae8578c90214093
dukc <dukkcc@proton.me>
2025-02-24 16:55:04 -0800

    init unpreview content

--- apps/docs/src/content.config.ts
+++ apps/docs/src/content.config.ts
@@ -1,28 +1,10 @@
+import { defineCollection } from "astro:content";
+import { PreviewSchema } from "@korhq/unpreview";
 import { glob } from "astro/loaders";
-import { defineCollection, z } from "astro:content";
 
-const DocsSchema = z.object({
-	title: z.string(),
-	directory: z.optional(z.boolean()),
-	section: z.optional(z.boolean()),
-	order: z.optional(z.number()),
+const previews = defineCollection({
+	loader: glob({ pattern: "**/*.{json,mdx}", base: "./src/content/previews" }),
+	schema: PreviewSchema,
 });
 
-const design = defineCollection({
-	loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/design" }),
-	schema: DocsSchema,
-});
-const api = defineCollection({
-	loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/api" }),
-	schema: DocsSchema,
-});
-const biblejson = defineCollection({
-	loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/biblejson" }),
-	schema: DocsSchema,
-});
-
-export const collections = {
-	design: design,
-	api: api,
-	biblejson: biblejson,
-};
+export const collections = { previews };
