5710cdfb1e3cb3ad81b34ff0a69e8de5e46615e2
dukc <dukkcc@proton.me>
2025-05-03 12:03:40 -0700

    asdasd

--- apps/web-app/src/components/bible/ControlBar.tsx
+++ apps/web-app/src/components/bible/ControlBar.tsx
@@ -8,10 +8,10 @@ export function ControlBar() {
         <p className="text-body">BSB</p>
         <div className="bg-accent absolute bottom-[-70%] left-0 -z-50 h-[2rem] w-[5rem] rounded-full opacity-50 blur-[1.5rem]" />
       </div>
-      <div className="rounded-4 border-stroke bg-fg-1 shadow-popup backdrop-blur-popup flex h-full items-center gap-12 border px-20 first:rounded-l-full last:rounded-r-full">
+      {/* <div className="rounded-4 border-stroke bg-fg-1 shadow-popup backdrop-blur-popup flex h-full items-center gap-12 border px-20 first:rounded-l-full last:rounded-r-full">
         <IconAlarm stroke={1.5} className="text-text size-24" />
         <IconMaximize stroke={1.5} className="text-text size-24" />
-      </div>
+     </div> */}
     </div>
   );
 }
